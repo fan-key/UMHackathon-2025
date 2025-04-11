88ae4432e27a4fd3a3a006e852b89391e6f79fac
Fankey <arfan.izanagi456@gmail.com>
2025-04-11 21:00:10 +0800

    added stuff

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # UMHackathon-2025
 
 github repo for 5 BUDAK BUJANG 
+
+
+before running this program, use your own API key and install the dependencies:
+pip install -r requirements.txt
---
+++ api/fetch_netflow.py
@@ -0,0 +1,46 @@
+import time
+import os
+import pandas as pd
+import requests
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+
+def fetch_netflow_data():
+    end_time = int(time.time() * 1000)
+    start_time = end_time - 86400 * 1000 
+
+    params = {
+        "exchange": "okx",
+        "window": "hour",
+        "end_time": end_time,
+        "limit": 1000,
+        "flatten": True
+    }
+    try:
+        response = requests.get(
+            "https://api.datasource.cybotrade.rs/cryptoquant/btc/exchange-flows/netflow?exchange=okx&window=hour",
+            headers={
+            "X-API-Key": API_KEY
+            },
+            params=params,
+        )
+
+        data = response.json()
+        df_netflow = pd.DataFrame(data['data'])
+
+        output_directory = 'data'
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+        
+        file_path = os.path.join(output_directory, 'netflow_data.csv')
+        df_netflow.to_csv(file_path, index=False)
+
+        print("netflow data fetched and saved to data/netflow_data.csv")
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching netflow data: {e}")
+
